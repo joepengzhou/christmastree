372a9ea087b738b328e8e2ffdc3ef81a6810650f
Joe Zhou <joe.zhou@v867mid7.identification7.com>
2014-06-12 09:26:17 -0700

    change port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var http = require('http');
 
 //Get the environment variables we need.
 var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 
 var express = require('express')
